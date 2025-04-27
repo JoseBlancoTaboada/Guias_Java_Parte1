fcb58eed8cb80108d1ef26bdfe88d8d49ab1478c
JoseBlancoTaboada <jose.blancotaboada@unicolombo.edu.co>
2025-04-27 03:21:20 -0500

    Añadiendo Guía23

--- Guia23/src/guia23/Guia23.java
+++ Guia23/src/guia23/Guia23.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package guia1;
+package guia23;
 
 /**
  *
- * @author elhac
+ * @author USER
  */
-public class Guia1 {
+public class Guia23 {
 
     /**
      * @param args the command line arguments
